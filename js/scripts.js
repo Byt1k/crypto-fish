fb125a5386900756c7f888197f2e4510ccb8e05b
Byt1k <byt1kip82@gmail.com>
2022-03-14 22:24:18 +0300

    added footer icons and mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,32 @@ let hideCookiesBlock = () => {
   cookiesBlock.classList.remove('active');
 }
 
-cookiesOk.addEventListener('click', hideCookiesBlock);
-cookiesDecline.addEventListener('click', hideCookiesBlock);
+if (cookiesOk) {
+  cookiesOk.addEventListener('click', hideCookiesBlock);
+}
+if (cookiesDecline) {
+  cookiesDecline.addEventListener('click', hideCookiesBlock);
+}
 //
 
+
+// Мобильное меню
+
+let mobileMenuOpen = document.querySelector('.head-string__burger');
+let mobileMenuClose = document.querySelector('.mobile-menu__close');
+let mobileMenu = document.querySelector('.mobile-menu');
+let mobileMenuList = document.querySelector('.mobile-menu__list');
+
+mobileMenuOpen.addEventListener('click', () => {
+  mobileMenu.classList.add('active');
+});
+
+mobileMenuClose.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+});
+
+[].forEach.call(mobileMenuList.children, function (item) {
+  item.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  })
+});
